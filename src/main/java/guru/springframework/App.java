2b3818ff52896c48f787641d3e91fef35d6e0aba
CarlosGR95 <cgramos@minsait.com>
2022-01-05 17:41:07 +0100

    First commit

---
+++ src/main/java/guru/springframework/App.java
@@ -0,0 +1,15 @@
+package guru.springframework;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println(StringUtils.capitalize("hello world"));
+    }
+}
