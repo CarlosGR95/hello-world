7b53e66ee3eed87e51078ab4d628b7a5819f3ad1
CarlosGR95 <cgramos@minsait.com>
2022-01-05 18:38:00 +0100

    adding pojo test

---
+++ src/test/java/guru/springframework/AppTest.java
@@ -0,0 +1,16 @@
+package guru.springframework;
+/**
+ * Created by jt on 2018-12-07.
+ */
+public class AppTest {
+
+    public void testHello(){
+
+        System.out.println("TEST - Hello...");
+    }
+
+    public void testWorld() {
+        System.out.println("TEST - World!!!!");
+    }
+
+}
